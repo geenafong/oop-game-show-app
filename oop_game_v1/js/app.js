e8c42b5c63d82ac55b20e77380f2939f3e5973e7
geenafong <geenafong@gmail.com>
2018-09-27 20:05:03 -0700

    save

--- oop_game_v1/js/app.js
+++ oop_game_v1/js/app.js
@@ -22,12 +22,15 @@ $('#btn__reset').click(function(){
 // markButton(): this function is called when a player selects a letter. It disables the button on the onscreen keyboard and calls the handleInteraction() method of the Game class.
 // Add event listeners to each of the keyboard buttons, so that clicking a button calls the markButton() function.
 function markButton(){     
-$('.key').click(function(){
-    ($(this).attr('disabled', 'disabled'))
+    $(this).attr('disabled', 'disabled');
     game.handleInteraction();
-});
 }
-markButton();
+
+$('.key').click(function(){
+    markButton();
+});
+
+
 
 
 
--- oop_game_v1/js/game.js
+++ oop_game_v1/js/game.js
@@ -14,6 +14,7 @@ class Game{
 
     // startGame(): calls the getRandomPhrase() method, and adds that phrase to the board by calling the Phrase class' addPhraseToDisplay() method.
     startGame(){
+       this.missed = 0;
        let randomPhrase = this.getRandomPhrases();
        randomPhrase.addPhraseToDisplay(randomPhrase);
     }
@@ -21,38 +22,53 @@ class Game{
     //method checks to see if the button clicked mathches a letter in the phrase
         //if not, then call the removeLife() method
     handleInteraction(){
-        this.phrases[0].checkLetter();
-        if ($(this).hasClass('letters in phrase')){
-            this.checkForWin();
-        } else {
-            this.removeLife();
-        }
+      let checkWin = this;
+      let heart = this;
+      this.phrases[0].checkLetter();
+      $('#qwerty button').click(function(){
+            if ($(this).hasClass('letters in phrase')){
+                $(this).css("background", "#5b85b7");
+                $('.rightLetter').css("background", "#4ac0d5");
+                checkWin.checkForWin();
+            } else {
+                $(this).css("background", "#f7a964");
+                heart.removeLife();
+            }
+        });
     }
     
     //removeLife(): this method removes a life, removes a heart from the board, and, if the player is out of lives, ends the game.
     removeLife(){
-       $('.tries:first').remove();
-       this.missed += 1
-
-       if(this.missed === 5){
-           this.gameOver();
-       }
+        this.missed +=1
+       $('#scoreboard img:first').remove();
+       
     }
 
-    //checkForWin(): this method checks to see if the player has selected all of the letters.
+    // //checkForWin(): this method checks to see if the player has selected all of the letters.
      checkForWin(){
-         let win = false
-         if (this.missed === 5) {
-             this.gameOver();
-         } else if (currentPhrase.textContent() == currentPhrase){
-            win = true;
-         }
-     }
+         if($('.rightLetter').length === $('.letter').length){
+            $('#overlay').show();
+            // $('#game-over-message').text('You Win!');
+            //  $('#overlay').addClass('win');
+            //  $('#overlay').attr('');
+         } else {
+                this.gameOver();
+            }
+        }
+         
 
      //gameOver(): this method displays a message if the player wins or a different message if they lose.
-     gameOver(message){
-		document.getElementById('game-over-message').style.display = 'block';
-        document.getElementById('game-over-message').textContent = message;
-    }
+     gameOver(){
+        let letters = $('.rightLetter');
+         if(letters.length === 0){
+             $('#game-over-message').text('You Win!');
+             $('#overlay').addClass('win');
+             $('#overlay').attr('');
+         } else if(this.missed === 5){
+            $('#game-over-message').text('You Lose!');
+            $('#overlay').addClass('lose');
+            $('#overlay').attr('');
+         }
+     }
 
 }
